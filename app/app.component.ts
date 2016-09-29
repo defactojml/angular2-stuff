b295ed48c1f653203385557dc11fc3067aa711c8
jean-michel.legrand <Fluent2015>
2016-09-29 14:08:28 +0200

    Template string + one way binding + two way data binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,15 +7,23 @@ export class Hero {
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1> <h2>{{hero.name}} details </h2>'
-
+    template: `
+        <h1>{{title}}</h1> 
+        <h2>{{hero.name}} details </h2> 
+        <div> <label>id: </label> {{hero.id}}</div> 
+        <div> 
+            <label>name: </label> 
+            <!--input value="{{hero.name}}" placeholder="enter ur name "-->
+            <input [(ngModel)]="hero.name" placeholder="name">
+        </div>
+        `
 })
 
 export class AppComponent {
     title = 'Tour of Heroes';
     hero: Hero = {
         id: 1,
-        name: 'Windstomr'
+        name: 'JML Windstorm'
     };
 }
 
