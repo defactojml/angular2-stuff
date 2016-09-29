19ef7ebe6d9270a6e7e684cefeb04dbcc17b1c3d
jean-michel.legrand <Fluent2015>
2016-09-29 14:50:07 +0200

    Master Detail - Have a list of heroes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from "@angular/core";
+import {Component} from "@angular/core";
+
+
+const HEROES: Hero[] = [
+    {id: 2, name: 'Michel PLATINI'},
+    {id: 3, name: 'Michel HIDALGO'},
+    {id: 4, name: 'Michel MEZY'},
+    {id: 5, name: 'Daniel BRAVO'},
+    {id: 6, name: 'Christian KAREMBEU'},
+    {id: 7, name: 'Zinedine ZIDANE'}
+];
 
 export class Hero {
     id: number;
@@ -9,22 +19,70 @@ export class Hero {
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1> 
-        <h2>{{hero.name}} details </h2> 
-        <div> <label>id: </label> {{hero.id}}</div> 
+        <h2> My Heroes </h2> 
         <div> 
-            <label>name: </label> 
-            <!--input value="{{hero.name}}" placeholder="enter ur name "-->
-            <input [(ngModel)]="hero.name" placeholder="name">
+            <ul class="heroes">
+                <li *ngFor="let hero of heroes">
+                    <span class="badge"> {{hero.id}}</span>{{hero.name}}
+                </li>
+            </ul>
         </div>
-        `
+        `,
+    styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
 
 export class AppComponent {
     title = 'Tour of Heroes';
-    hero: Hero = {
-        id: 1,
-        name: 'JML Windstorm'
-    };
+    heroes = HEROES;
+
 }
 
 
