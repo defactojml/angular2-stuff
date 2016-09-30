792e3e24e93fb1f7277a73c1532effc7f6498e71
jean-michel.legrand <Fluent2015>
2016-09-30 13:50:04 +0200

    Services  - Add a mocked class

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -4,15 +4,7 @@
 
 import {Injectable} from '@angular/core';
 import {Hero} from "./hero";
-
-const HEROES: Hero[] = [
-    {id: 2, name: 'Michel PLATINI'},
-    {id: 3, name: 'Michel HIDALGO'},
-    {id: 4, name: 'Michel MEZY'},
-    {id: 5, name: 'Daniel BRAVO'},
-    {id: 6, name: 'Christian KAREMBEU'},
-    {id: 7, name: 'Zinedine ZIDANE'}
-];
+import {HEROES} from './mock-heroes';
 
 @Injectable()
 export class HeroService {
---
+++ app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import {Hero} from "./hero";
+/**
+ * Created by jean-michel.legrand on 30/09/2016.
+ */
+
+export const HEROES: Hero[] = [
+    {id: 2, name: 'Michel PLATINI'},
+    {id: 3, name: 'Michel HIDALGO'},
+    {id: 4, name: 'Michel MEZY'},
+    {id: 5, name: 'Daniel BRAVO'},
+    {id: 6, name: 'Christian KAREMBEU'},
+    {id: 7, name: 'Zinedine ZIDANE'}
+];
