2ad3c7d7fa1e5f9ce257534cd6afee14aa10c439
jean-michel.legrand <Fluent2015>
2016-10-07 16:07:20 +0200

    Routing   - Create HeroesComponent & transform AppComponent into shell component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,25 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
-
-import {AppComponent}  from './app.component';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroesComponent} from "./heroes.component";
+import {AppComponent} from "./app.component";
+import {HeroService} from "./hero.service";
 
 @NgModule({
-  imports: [
-      BrowserModule,
-      FormsModule
-  ],
-  declarations: [
-      AppComponent,
-      HeroDetailComponent
-  ],
-  bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent,
+        HeroesComponent,
+        HeroDetailComponent
+    ],
+    providers: [HeroService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+
+export class AppModule {
+}
