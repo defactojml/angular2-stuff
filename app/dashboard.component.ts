2dcbec3469a67d201af1d9594c67248f1e8c1374
jean-michel.legrand <Fluent2015>
2016-10-11 10:23:42 +0200

    Routing - Add a dedicated html template using templateUrl

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -5,8 +5,9 @@
 import {Component} from "@angular/core";
 
 @Component({
+    moduleId: module.id,
     selector: 'my-dashboard',
-    template: '<h3>My Dashboard</h3>'
+    templateUrl: 'dashboard.component.html'
 })
 
 export class DashboardComponent {
