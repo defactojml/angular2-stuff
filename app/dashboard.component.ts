f9f3a077a0a77fb02542d38a8f02724891bd51f1
jean-michel.legrand <Fluent2015>
2016-10-11 10:50:33 +0200

    Routing - Refine the dedicated html template to disply the first 4 items

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -3,6 +3,8 @@
  */
 
 import {Component} from "@angular/core";
+import {Hero} from "./hero";
+import {HeroService} from "./hero.service";
 
 @Component({
     moduleId: module.id,
@@ -11,5 +13,19 @@ import {Component} from "@angular/core";
 })
 
 export class DashboardComponent {
+
+    heroes: Hero[] = [];
+
+    constructor(private heroService: HeroService) {}
+
+    ngOnInit(): void {
+        this.heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.slice(0, 4));
+    }
+
+    onSelect(hero: Hero): void {
+        console.info('JM - onSelect');
+        console.info('JM - hero', JSON.stringify(hero));
+    }
 
 }
